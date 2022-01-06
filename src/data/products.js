f899eeabef49ce9bb528c445382a1e75eb6071e8
unknown <bakialakshmi.g.d@.com>
2022-01-06 20:54:24 +0530

    Cart React

---
+++ src/data/products.js
@@ -0,0 +1,46 @@
+const data = [
+  {
+    id: 1,
+    name: 'Sessel Morteen',
+    price: '12,750',
+    currency: 'Rs',
+    image: 'images/01.jpg',
+  },
+  {
+    id: 2,
+    name: 'Sessel Sofie',
+    price: '9999',
+    currency: 'Rs',
+    image: 'images/02.jpg',
+  },
+  {
+    id: 3,
+    name: 'Sessel Anna',
+    price: '6450',
+    currency: 'Rs',
+    image: 'images/03.jpg',
+  },
+  {
+    id: 4,
+    name: 'Sessel Rovigo',
+    price: '14,999',
+    currency: 'Rs',
+    image: 'images/04.jpg',
+  },
+  {
+    id: 5,
+    name: 'Sessel Peacock',
+    price: '13,000',
+    currency: 'Rs',
+    image: 'images/05.jpg',
+  },
+  {
+    id: 6,
+    name: 'Sessel Little',
+    price: '8888',
+    currency: 'Rs',
+    image: 'images/06.jpg',
+  } 
+];
+
+export default data;
